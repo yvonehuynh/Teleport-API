53ca917479c14546dda42c6007807e1ff80dab40
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-16 10:25:20 -0500

    got substring before first comma function

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -13,15 +13,20 @@ $(document).ready(function(){
     var input = document.getElementById('searchTextField');
     var options = {
         bounds: defaultBounds,
-        types: ['(cities)'],
+        types: ['(regions)'],
     };
     var autocomplete = new google.maps.places.Autocomplete(input, options);
     // end autocomplete
 
+
+    // when form submits, get the value of input up to the first comma
     $(".main-search").on("submit", function(e){
         e.preventDefault();
        // const input = $("#searchTextField").val();
-        console.log($("#searchTextField").val());
+       let input = $("#searchTextField").val();
+       input = input.substring(0, input.indexOf(","));
+        /* console.log($("#searchTextField").val()); */
+        console.log(input);
     })
 
 });// end doc ready
