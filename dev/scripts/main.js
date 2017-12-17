f98a4052adedde8cfb22222d8ddda3d321a9743e
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-17 09:39:45 -0500

    added double ajax requests

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -17,10 +17,21 @@ getTeleport.apiRequest = function(city){
     }) // end .fail promise
 }; // end ajax 
 
+getTeleport.secondApiRequest = function(compareCity) {
+    $.ajax({
+        url: `https://api.teleport.org/api/urban_areas/slug:${compareCity}/scores/`,
+        method: "GET",
+        dataTYpe: "JSON"
+    })
+    .then(function(secondResponse){
+        console.log(secondResponse);
+        getTeleport.displaySecondScore(secondResponse);
+    });
+};
 // progress bar
 function progress(id, percentage) {
     $(`#${id}`).LineProgressbar({
-        percentage: percentage,
+        percentage: percentage*10,
         duration: 1000,
         fillBackgroundColor: '#3498db',
         backgroundColor: '#EEEEEE',
@@ -48,8 +59,25 @@ getTeleport.displayScore = function(response){
     });
 };
 
+getTeleport.displaySecondScore = function (secondResponse) {
+    let scoreArray = secondResponse;
+    $(".score2").append(
+        `<p>${secondResponse.summary}</p>`
+    );
+    scoreArray.categories.forEach(function (newArray, index) {
+        $(".score2").append(
+            `<li>
+                <h2>${newArray.name}</h2>
+                <p>${newArray.score_out_of_10}</p>
+                <div id="${index}"></div>
+            </li>`
+        );
+        progress(index, newArray.score_out_of_10);
+    });
+};
+
 // function to initialize api request via user input
-const userInput = () => {
+/* const userInput = () => {
     // when form submits, get the value of input up to the first comma
     $(".main-search").on("submit", function (e) {
         e.preventDefault();
@@ -58,11 +86,53 @@ const userInput = () => {
         console.log(city);
         getTeleport.apiRequest(city);
     });
-}; // end userInput funciton
+};  */// end userInput funciton
+
+const userInput = (form, input) => {
+    // when form submits, get the value of input up to the first comma
+    $(".main-search").on("submit", function (e) {
+        e.preventDefault();
+        let city = $("#searchTextField").val();
+        city = city.substring(0, city.indexOf(",")).toLowerCase().replace(/ /, "-"); 
+        console.log(city);
+        getTeleport.apiRequest(city);
+    });
+};
+
+const secondUserInput =()=> {
+    // when form submits, get the value of input up to the first comma
+    $(".second-search").on("submit", function (e) {
+        e.preventDefault();
+        let city = $("#secondTextField").val();
+        city = city.substring(0, city.indexOf(",")).toLowerCase().replace(/ /, "-");
+        console.log(city);
+        getTeleport.secondApiRequest(city);
+    });
+};
+
+
+function autocompleteInput(inputId){
+    // autocomplete
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(-90, -180),
+        new google.maps.LatLng(90, 180));
+        
+        var input = document.getElementById(inputId);
+        var options = {
+            bounds: defaultBounds,
+            types: ['(regions)'],
+        };
+        var autocomplete = new google.maps.places.Autocomplete(input, options);
+        // end autocomplete
+}
 
 getTeleport.init=()=>{
+/*     userInput(".main-search", "#searchTextField");
+    userInput(".second-search", "#secondTextField"); */
     userInput();
-
+    secondUserInput();
+    autocompleteInput("searchTextField");
+    autocompleteInput("secondTextField");
 }; // end init
 
 
@@ -70,18 +140,5 @@ getTeleport.init=()=>{
 $(document).ready(function(){
     
     getTeleport.init();
-    // autocomplete
-    var defaultBounds = new google.maps.LatLngBounds(
-        new google.maps.LatLng(-90, -180),
-        new google.maps.LatLng(90, 180));
-        
-        var input = document.getElementById('searchTextField');
-        var options = {
-            bounds: defaultBounds,
-            types: ['(regions)'],
-        };
-        var autocomplete = new google.maps.places.Autocomplete(input, options);
-        // end autocomplete
-        // bar indicator 
 
 });// end doc ready
